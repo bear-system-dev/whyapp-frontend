922fdbd884fd66166873f5930f0b879c1ae0e367
GEAN RT <geanrtofc@gmail.com>
2024-02-05 23:50:25 -0300

    adição do componente de login

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,10 +1,19 @@
 import { ButtonExample } from '@/components/Button'
+import { useState } from 'react'
+import Login from '../Login/Login'
 
 export function App() {
+  const [loggedIn] = useState<boolean>(false)
   return (
     <div>
-      <h1>Hello, World!</h1>
-      <ButtonExample />
+      {loggedIn ? (
+        <>
+          <h1>Hello, World!</h1>
+          <ButtonExample />
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
   )
 }
