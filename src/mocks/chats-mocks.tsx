49fe715efeefdac60a19711d7819a3188bed3532
artenlf <felipe.arten@gmail.com>
2024-02-10 15:43:55 -0300

    refactor: refactor App main layout

--- src/mocks/chats-mocks.tsx
+++ src/mocks/chats-mocks.tsx
@@ -1,28 +1,19 @@
 import BubbleChat from '@/components/bubblechat'
 import { Flex } from 'antd'
 
-export const ChatContainer = () => {
+export const MockChats = () => {
   return (
-    <Flex
-      vertical
-      style={{
-        gap: '2rem',
-        height: 'calc(100% - 11.5rem)',
-        width: '100%',
-        padding: '3rem calc(15.5rem + 6rem) 3rem 15.5rem ',
-        position: 'fixed',
-        zIndex: 3,
-        alignItems: 'flex-end',
-      }}
-    >
-      <BubbleChat
-        color="#3F7B40"
-        username="calvão buena"
-        chatprivate={true}
-        mensage="Hello everyone guys 😊 this is component chat conversation!"
-        time="20:58"
-      />
-      <div style={{ alignSelf: 'start' }}>
+    <>
+      <Flex style={{ alignSelf: 'end' }}>
+        <BubbleChat
+          username="calvão buena"
+          color="#3F7B40"
+          chatprivate={true}
+          mensage="Hello everyone guys 😊 this is component chat conversation!"
+          time="20:58"
+        />
+      </Flex>
+      <Flex>
         <BubbleChat
           image="031b68882265722dede1080a200f015a.jpg"
           username="calvão buena"
@@ -32,16 +23,80 @@ export const ChatContainer = () => {
           mensage="Hello everyone guys 😊 this is a component chat conversation!"
           time="20:58"
         />
-      </div>
-      <BubbleChat
-        image="031b68882265722dede1080a200f015a.jpg"
-        username="motoplay_br"
-        cargo="member"
-        color="#3F7B40"
-        chatprivate={false}
-        mensage="Hello everyone guys 😊 this is a component chat conversation!"
-        time="20:58"
-      />
-    </Flex>
+      </Flex>
+      <Flex style={{ alignSelf: 'end' }}>
+        <BubbleChat
+          image="031b68882265722dede1080a200f015a.jpg"
+          username="motoplay_br"
+          cargo="member"
+          color="#3F7B40"
+          chatprivate={false}
+          mensage="Hello everyone guys 😊 this is a component chat conversation!"
+          time="20:58"
+        />
+      </Flex>
+      <Flex style={{ alignSelf: 'end' }}>
+        <BubbleChat
+          username="calvão buena"
+          color="#3F7B40"
+          chatprivate={true}
+          mensage="Hello everyone guys 😊 this is component chat conversation!"
+          time="20:58"
+        />
+      </Flex>
+      <Flex>
+        <BubbleChat
+          image="031b68882265722dede1080a200f015a.jpg"
+          username="calvão buena"
+          cargo="admin"
+          color="#434455"
+          chatprivate={false}
+          mensage="Hello everyone guys 😊 this is a component chat conversation!"
+          time="20:58"
+        />
+      </Flex>
+      <Flex style={{ alignSelf: 'end' }}>
+        <BubbleChat
+          image="031b68882265722dede1080a200f015a.jpg"
+          username="motoplay_br"
+          cargo="member"
+          color="#3F7B40"
+          chatprivate={false}
+          mensage="Hello everyone guys 😊 this is a component chat conversation!"
+          time="20:58"
+        />
+      </Flex>
+      <Flex style={{ alignSelf: 'end' }}>
+        <BubbleChat
+          username="calvão buena"
+          color="#3F7B40"
+          chatprivate={true}
+          mensage="Hello everyone guys 😊 this is component chat conversation!"
+          time="20:58"
+        />
+      </Flex>
+      <Flex>
+        <BubbleChat
+          image="031b68882265722dede1080a200f015a.jpg"
+          username="calvão buena"
+          cargo="admin"
+          color="#434455"
+          chatprivate={false}
+          mensage="Hello everyone guys 😊 this is a component chat conversation!"
+          time="20:58"
+        />
+      </Flex>
+      <Flex style={{ alignSelf: 'end' }}>
+        <BubbleChat
+          image="031b68882265722dede1080a200f015a.jpg"
+          username="motoplay_br"
+          cargo="member"
+          color="#3F7B40"
+          chatprivate={false}
+          mensage="Hello everyone guys 😊 this is a component chat conversation!"
+          time="20:58"
+        />
+      </Flex>
+    </>
   )
 }
