19118a59d5ac71b18b116788565ba48f78da2fe5
artenlf <felipe.arten@gmail.com>
2024-04-04 20:56:16 -0300

    feat/refactor: chat messages functions and refactor

--- src/components/InputBar/index.tsx
+++ src/components/InputBar/index.tsx
@@ -1,24 +1,61 @@
 import emojiIcon from '@/assets/emojiIcon.png'
 import { ChatContext } from '@/contexts/chatContext'
+import { api } from '@/lib/api'
 import { useChatSocket } from '@/utils/hooks/useChatSocket'
+import { useGroupChatSocket } from '@/utils/hooks/useGroupChatSocket'
 import { PaperClipOutlined, SendOutlined } from '@ant-design/icons'
 import { Button, Flex, Input, Space } from 'antd'
 import { EmojiClickData } from 'emoji-picker-react'
+import Cookies from 'js-cookie'
 import { ChangeEvent, KeyboardEvent, useContext, useState } from 'react'
 import { resetButtonStyles } from './../../mocks/mockUserArray'
 import { EmojiLibrary } from './emojiPicker'
 import './style.css'
 
 export function InputBar() {
-  const { recipient } = useContext(ChatContext)
-  const { socket } = useChatSocket()
+  const { recipient, recipientGroup } = useContext(ChatContext)
+  const { socket, userId } = useChatSocket()
+  const { recipientGroupId } = useGroupChatSocket()
+
   const [inputValue, setInputValue] = useState<string>('')
   const [showEmojis, setShowEmojis] = useState(false)
+
+  const newMessage = async (value: string) => {
+    const token = Cookies.get('token')
+
+    const response = await api.post(
+      'group-messages',
+      {
+        mensagem: value,
+        grupoId: recipientGroupId,
+        usuarioId: userId,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    )
+
+    socket?.emit('newGroupMessage', 'response.data.id')
+    return response
+  }
 
   const handleSendMessage = () => {
     if (!inputValue.trim()) return
 
-    socket?.emit('newMessage', inputValue)
+    if (recipient) {
+      socket?.emit('newMessage', inputValue)
+    }
+
+    newMessage(inputValue)
+
+    console.log(inputValue)
+
+    // sendNewGroupMessageMutation.mutate({
+    //   mensagem: inputValue,
+    //   groupId: recipientGroupId,
+    // })
 
     setInputValue('')
   }
@@ -46,7 +83,7 @@ export function InputBar() {
           type="text"
           style={{ ...resetButtonStyles, height: 30 }}
           onClick={handleOpenEmojiDrawer}
-          disabled={!recipient}
+          disabled={!recipient && !recipientGroup}
         >
           <img src={emojiIcon} alt="emoji icon" height={'100%'} />
         </Button>
@@ -61,7 +98,7 @@ export function InputBar() {
             alignItems: 'center',
             justifyContent: 'center',
           }}
-          disabled={!recipient}
+          disabled={!recipient && !recipientGroup}
         />
       </Flex>
       <Space.Compact className="input-bar__container">
@@ -71,7 +108,7 @@ export function InputBar() {
           value={inputValue}
           onChange={handleInputOnChange}
           onKeyDown={handleOnKeyDown}
-          disabled={!recipient}
+          disabled={!recipient && !recipientGroup}
         />
         <Button
           className="send-button"
@@ -79,7 +116,7 @@ export function InputBar() {
           size="large"
           type="text"
           onClick={handleSendMessage}
-          disabled={!recipient}
+          disabled={!recipient && !recipientGroup}
         />
       </Space.Compact>
     </div>
