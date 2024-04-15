ef1f6deadac31451219909aa44b72a12cbedbe22
artenlf <felipe.arten@gmail.com>
2024-04-14 21:18:04 -0300

    refactor/fix: group chat hooks

--- src/utils/hooks/useGroupChatSocket.ts
+++ src/utils/hooks/useGroupChatSocket.ts
@@ -1,18 +1,40 @@
+// useGroupChatSocket.ts
+import { apiFunction } from '@/api/api'
 import { ChatContext } from '@/contexts/chatContext'
+import { GroupMessage } from '@/model/GroupMessageModel'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import Cookies from 'js-cookie'
 import { useContext, useEffect, useState } from 'react'
 import { Socket, io } from 'socket.io-client'
 
 export const useGroupChatSocket = () => {
-  const { recipientGroup } = useContext(ChatContext)
+  const { recipientGroup, setGroupMessages } = useContext(ChatContext)
   const [socket, setSocket] = useState<Socket | null>(null)
+  const queryClient = useQueryClient()
 
   const URL = `${import.meta.env.VITE_APP_BASE_URL}/grupos`
   const userId = Cookies.get('userId')
   const recipientGroupId = recipientGroup?.id
 
-  const sortByFirst = [userId, recipientGroupId].sort()
-  const chatId = sortByFirst.join('')
+  const {
+    data: groupMessagesData,
+    isLoading: groupMessagesLoading,
+    error: groupMessagesError,
+    refetch,
+  } = useQuery<GroupMessage[]>({
+    queryKey: ['group-messages', recipientGroupId],
+    queryFn: () => apiFunction.getGroupMessage({ grupoId: recipientGroupId }),
+    enabled: !!recipientGroupId,
+  })
+
+  useEffect(() => {
+    if (recipientGroupId) {
+      queryClient.removeQueries({
+        queryKey: ['group-messages'],
+      })
+      refetch()
+    }
+  }, [recipientGroupId])
 
   useEffect(() => {
     if (userId && recipientGroupId) {
@@ -23,13 +45,47 @@ export const useGroupChatSocket = () => {
         },
       })
 
+      newSocket.on('connect', () => {
+        if (recipientGroupId) {
+          newSocket.emit('join group', recipientGroupId)
+          if (groupMessagesData) {
+            setGroupMessages(groupMessagesData)
+          }
+        }
+      })
+
+      newSocket.on('newGroupMessage', (newGroupMessage: GroupMessage) => {
+        setGroupMessages((previousGroupMessages) => {
+          const messageExists = previousGroupMessages.some(
+            (message) => message.id === newGroupMessage.id,
+          )
+
+          if (messageExists) {
+            return previousGroupMessages
+          }
+
+          return [...previousGroupMessages, newGroupMessage]
+        })
+      })
+
+      newSocket.on('error', (data) => {
+        console.log(data)
+      })
+
       setSocket(newSocket)
-    }
 
-    return () => {
-      socket?.disconnect()
+      return () => {
+        newSocket.disconnect()
+      }
     }
-  }, [recipientGroup])
+  }, [recipientGroup, recipientGroupId, groupMessagesData])
 
-  return { recipientGroupId, socket, chatId, userId }
+  return {
+    recipientGroupId,
+    socket,
+    userId,
+    groupMessagesData,
+    groupMessagesLoading,
+    groupMessagesError,
+  }
 }
