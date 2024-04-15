e48138b31efdb2964383d0d7795438c14da8f387
artenlf <felipe.arten@gmail.com>
2024-04-15 12:51:31 -0300

    fix: group socket

--- src/utils/hooks/useGroupChatSocket.ts
+++ src/utils/hooks/useGroupChatSocket.ts
@@ -2,7 +2,7 @@
 import { apiFunction } from '@/api/api'
 import { ChatContext } from '@/contexts/chatContext'
 import { GroupMessage } from '@/model/GroupMessageModel'
-import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { useQuery } from '@tanstack/react-query'
 import Cookies from 'js-cookie'
 import { useContext, useEffect, useState } from 'react'
 import { Socket, io } from 'socket.io-client'
@@ -10,7 +10,6 @@ import { Socket, io } from 'socket.io-client'
 export const useGroupChatSocket = () => {
   const { recipientGroup, setGroupMessages } = useContext(ChatContext)
   const [socket, setSocket] = useState<Socket | null>(null)
-  const queryClient = useQueryClient()
 
   const URL = `${import.meta.env.VITE_APP_BASE_URL}/grupos`
   const userId = Cookies.get('userId')
@@ -20,21 +19,11 @@ export const useGroupChatSocket = () => {
     data: groupMessagesData,
     isLoading: groupMessagesLoading,
     error: groupMessagesError,
-    refetch,
   } = useQuery<GroupMessage[]>({
     queryKey: ['group-messages', recipientGroupId],
     queryFn: () => apiFunction.getGroupMessage({ grupoId: recipientGroupId }),
     enabled: !!recipientGroupId,
   })
-
-  useEffect(() => {
-    if (recipientGroupId) {
-      queryClient.removeQueries({
-        queryKey: ['group-messages'],
-      })
-      refetch()
-    }
-  }, [recipientGroupId])
 
   useEffect(() => {
     if (userId && recipientGroupId) {
@@ -69,7 +58,7 @@ export const useGroupChatSocket = () => {
 
       setSocket(newSocket)
     }
-  }, [recipientGroup, recipientGroupId, groupMessagesData])
+  }, [groupMessagesData])
 
   return {
     recipientGroupId,
