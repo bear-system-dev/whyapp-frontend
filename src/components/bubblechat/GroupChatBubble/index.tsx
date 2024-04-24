b4e657343931834b37b49cc11aea692088653616
artenlf <felipe.arten@gmail.com>
2024-04-23 22:17:40 -0300

    feat: add users Avatar and Name to Group Messages

---
+++ src/components/bubblechat/GroupChatBubble/index.tsx
@@ -0,0 +1,49 @@
+import { GroupMessage } from '@/model/GroupMessageModel'
+import { Flex } from 'antd'
+import Cookies from 'js-cookie'
+import React, { ReactNode } from 'react'
+import ProfileContact from '../../profile'
+import SentAt from '../components/sendAt'
+
+export interface GroupChatBubbleProps extends GroupMessage {
+  showProfileContact: boolean
+  children: ReactNode
+}
+
+const userId = Cookies.get('userId')
+
+const GroupChatBubble: React.FC<GroupChatBubbleProps> = ({
+  mensagem,
+  createdAt,
+  usuarioId,
+  showProfileContact,
+}) => {
+  return (
+    <Flex
+      vertical
+      style={{
+        gap: '0.5rem',
+      }}
+    >
+      {showProfileContact && usuarioId !== userId && (
+        <ProfileContact fromUserId={usuarioId} />
+      )}
+      <Flex
+        vertical
+        style={{
+          width: 'fit-content',
+          padding: '1rem',
+          backgroundColor: `${usuarioId === userId ? '#3F7B40' : '#434455'}`,
+          borderRadius: '14px',
+          color: '#FFFFFF',
+          marginLeft: '4rem',
+          wordWrap: 'break-word',
+        }}
+      >
+        {mensagem}
+        {createdAt && <SentAt time={createdAt} />}
+      </Flex>
+    </Flex>
+  )
+}
+export default GroupChatBubble
