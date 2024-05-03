e6b2416a8f1b863678df23e69f98f28db737a68c
artenlf <felipe.arten@gmail.com>
2024-05-02 23:24:54 -0300

    fix/styles: InputBar and ChatBubble body scroll bugs and change app bg

--- src/components/MenuGroup/style.ts
+++ src/components/MenuGroup/style.ts
@@ -3,7 +3,7 @@ export const MenuContainer: React.CSSProperties = {
   backgroundColor: 'rgba(18, 29, 40, 0.9)',
   backdropFilter: 'blur(2px)',
   position: 'fixed',
-  left: '66px',
+  left: '65px',
   bottom: '0',
   width: '300px',
   zIndex: 1,
@@ -12,7 +12,7 @@ export const MenuContainer: React.CSSProperties = {
 export const SettingsButtonStyle: React.CSSProperties = {
   background: 'transparent',
   position: 'fixed',
-  left: '65px',
-  top: '55px',
+  left: '74px',
+  top: '61px',
   zIndex: '999',
 }
