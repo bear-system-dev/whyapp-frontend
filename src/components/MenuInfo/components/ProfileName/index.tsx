74e93e9aa1f4c221f78d42fb8c54cc2f3aca3550
devdouglassilva <devdouglassilva@gmail.com>
2024-05-07 16:34:25 +0000

    Chore: Rename MenuProfile to ProfileMenu

--- src/components/MenuInfo/components/ProfileName/index.tsx
+++ src/components/MenuInfo/components/ProfileName/index.tsx
@@ -4,14 +4,12 @@ const NameProfile = ({ children }: { children: ReactNode }) => {
   return (
     <p
       style={{
-        color: 'white',
+        color: '#fff',
         margin: '0',
         width: 'max-content',
-        lineHeight: '100%',
-        fontWeight: '600',
+        fontWeight: '500',
         fontSize: '1rem',
-        textShadow: '0px 1px 4px #282828',
-        WebkitTextStroke: '0.25px #F8F8F8',
+        textTransform: 'capitalize'
       }}
     >
       {children}
